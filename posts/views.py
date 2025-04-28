14d6cb129dc3bc5546ae03b1c4ffebafc2f58144
Destrution99 <leogan888@gmail.com>
2025-04-28 23:23:08 +0100

    Fixed incoming change header

--- posts/views.py
+++ posts/views.py
@@ -7,17 +7,20 @@ from .forms import PostForm, CommentForm
 from communities.models import Community, Membership
 def post_list(request):
     filter_option = request.GET.get('filter', None)
-    search_query = request.GET.get('q', '')  # New: get search query
+    search_query = request.GET.get('q', '')  # Get search query
 
     if filter_option == 'my_posts' and request.user.is_authenticated:
+        # Filter posts by current user
         posts = Post.objects.filter(author=request.user).order_by('-created_at')
     elif request.user.is_authenticated:
+        # Get communities the user is a member of
         user_communities = request.user.communities.all()
         posts = Post.objects.filter(community__in=user_communities).order_by('-created_at')
     else:
+        # For non-authenticated users, show all posts
         posts = Post.objects.all().order_by('-created_at')
 
-    # New: Apply search filter
+    # Apply search filter
     if search_query:
         posts = posts.filter(
             Q(title__icontains=search_query) |
